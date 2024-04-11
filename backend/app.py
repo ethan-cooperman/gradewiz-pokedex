becb13acb2d50aa4c742cf1da86ab7c0422f8c7f
ethan-cooperman <ethan_cooperman@brown.edu>
2024-04-10 21:44:23 -0400

    feat: sqlite mocked database

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import requests
 app = Flask(__name__)
 
 @app.route("/get", methods=["GET"])
 def get_pokemon():
+    # Get the query parameters
     offset = request.args.get("offset", default=0, type=int)
     limit = request.args.get("limit", default=25, type=int)
     page = request.args.get("page", default=1, type=int)
@@ -13,13 +14,17 @@ def get_pokemon():
 
 @app.route("/search", methods=["GET"])
 def search_pokemon():
+    # Get the query parameters
     offset = request.args.get("offset", default=0, type=int)
     limit = request.args.get("limit", default=10000, type=int)
     page = request.args.get("page", default=1, type=int)
     keyword = request.args.get("keyword", default="", type=str)
     pokemon_response = requests.get(f"https://pokeapi.co/api/v2/pokemon?offset={offset + limit * (page - 1)}&limit={limit}")
+    # Check if the request was successful
     pokemon_response.raise_for_status()
+    # Convert the response to JSON
     pokemon_json = pokemon_response.json()
+    # Filter the JSON based on the keyword
     return_json = {}
     for key in pokemon_json:
         if key != "results":
@@ -34,6 +39,7 @@ def search_pokemon():
 
 @app.errorhandler(404)
 def page_not_found(error):
+
     return '<h1>Error 404<h1/> <div style="font-size: 14px; color: blue;">We unfortunately could not catch them all today</div>', 404
 
 @app.errorhandler(500)
---
+++ backend/create_table.py
@@ -0,0 +1,91 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def create_user(conn, user):
+    """
+    Create a new user into the users table
+    :param conn:
+    :param user:
+    :return: user id
+    """
+    sql = ''' INSERT INTO users(email,password)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, user)
+    return cur.lastrowid
+
+def create_favorite(conn, favorite):
+    """
+    Create a new favorite into the favorites table
+    :param conn:
+    :param favorite:
+    :return: favorite id
+    """
+    sql = ''' INSERT INTO favorites(user_id,pokemon_id)
+              VALUES(?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, favorite)
+    return cur.lastrowid
+
+def main():
+    database = "pokedex.db"
+
+    sql_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                        id integer PRIMARY KEY,
+                                        email text NOT NULL,
+                                        password text NOT NULL,
+                                        created datetime DEFAULT CURRENT_TIMESTAMP
+                                    ); """
+    
+    sql_favorites_table = """ CREATE TABLE IF NOT EXISTS favorites (
+                                        id integer PRIMARY KEY,
+                                        user_id integer NOT NULL,
+                                        pokemon_id integer NOT NULL,
+                                        created datetime DEFAULT CURRENT_TIMESTAMP,
+                                        FOREIGN KEY (user_id) REFERENCES users (id)
+                                    ); """
+    
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create users table
+        create_table(conn, sql_users_table)
+        create_table(conn, sql_favorites_table)
+        c = conn.cursor()
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
